100aaf991dd225bbdecea8c730d577d7566d256c
sannaasp <sanna.asp@hotmail.com>
2023-05-15 21:12:07 +0200

    update card component

--- frontend/src/components/productCard.tsx
+++ frontend/src/components/productCard.tsx
@@ -5,12 +5,14 @@ interface Product {
   id: number;
   title: string;
   subtitle: string;
+  description: string;
   img: string;
   price: number;
 }
 
 function Products() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [visibleProduct, setVisibleProduct] = useState<number>(3);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,28 +26,38 @@ function Products() {
     };
     fetchData();
   }, []);
+
+  const handleShowProducts = () => {
+    setVisibleProduct(visibleProduct + 3);
+  };
+
   return (
     <>
       {products.length > 0 ? (
         <ol className="Product-list">
-          {products.map((product) => (
+          {products.slice(0, visibleProduct).map((product) => (
             <li key={product.id}>
-              <div className="Vegetables-container">
-                {/* <Link to={`/VegView/${product.name}`}> */}
-                <h1>{product.title}</h1>
-                <p>{product.subtitle}</p>
-                <p>{product.price}</p>
+              <div className="Product-container">
                 <img
                   className="Product-image"
                   src={product.img}
                   alt={product.img}
                 />
+                {/* <Link to={`/VegView/${product.name}`}> */}
+                <h1>{product.title}</h1>
+                <p>{product.subtitle}</p>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+
                 {/* </Link> */}
               </div>
             </li>
           ))}
         </ol>
       ) : null}
+      {visibleProduct < products.length && (
+        <button onClick={handleShowProducts}>Show more</button>
+      )}
     </>
   );
 }
