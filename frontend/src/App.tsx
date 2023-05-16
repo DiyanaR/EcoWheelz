580602645f73be0ebd9cd3ad6bbd1c0d5a0e4c68
sannaasp <sanna.asp@hotmail.com>
2023-05-16 11:14:20 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ProductCard from "./components/ProductCard";
+// import ProductCard from "./components/ProductCard";
 import "./index.css";
 
 import {
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       {/* <Navbar /> */}
       <Outlet />
-      <ProductCard />
+      {/* <ProductCard /> */}
       {/* <Footer /> */}
     </div>
   );
