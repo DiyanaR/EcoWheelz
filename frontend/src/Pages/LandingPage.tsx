d5d8f75afa3d8d24a2b98a17cd759aec1118ab9f
Jovan <jovan.14@hotmail.com>
2023-05-22 12:31:15 +0200

    Added styled-components to landingPage

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -6,88 +6,82 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
   return (
     <>
-      {" "}
       <Main>
-        <h1>hello</h1>
-      </Main>
-      <div style={{ position: "relative", fontFamily: "Red Hat Display" }}>
-        <img
-          src={LandingImage}
-          alt="Min bild"
-          style={{
-            width: "1460px",
-            height: "1044px",
-            zIndex: 0,
-          }}
-        />
+        <div style={{ position: "relative", fontFamily: "Red Hat Display" }}>
+          <img className="hero-image" src={LandingImage} alt="Min bild" />
 
-        <div
-          style={{
-            position: "absolute",
-            top: "352px",
-            left: "301px",
-            width: "873px",
-            height: "306px",
-            backgroundColor: "rgba(0, 0, 0, 0.7)", // svart färg med 70% opacitet
-            border: "2px solid rgba(0, 0, 0, 0.7)",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              color: "white",
-              fontSize: "48px",
-              textAlign: "center",
-              // width: "873px",
-              // height: "306px",
-              top: "352px",
-              placeItems: "center",
-              marginBottom: "20px",
-            }}
-          >
-            Silent. Green. Ecowheelz.
-          </div>
-
-          <div
-            style={{ color: "white", fontSize: "16px", marginRight: "130px" }}
-          >
-            Sustainable transport made easy. Your green ride awaits!
-          </div>
-
-          <Link to="/productpage">
-            <button
+          <div className="hero-headline">
+            <div
               style={{
-                fontFamily: "Red Hat Display",
-                fontSize: "14px",
-                lineHeight: "19px",
-                textAlign: "left",
-                verticalAlign: "top",
-                color: "#faf9f8",
-                width: "109px",
-                height: "39px",
-                borderRadius: "16px",
-                borderColor: "#9ae5bd",
-                backgroundColor: "transparent",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginTop: "20px",
+                color: "white",
+                fontSize: "48px",
+                textAlign: "center",
+                // width: "873px",
+                // height: "306px",
+                top: "352px",
+                placeItems: "center",
+                marginBottom: "20px",
               }}
             >
-              Explore more
-            </button>
-          </Link>
-        </div>
+              Silent. Green. Ecowheelz.
+            </div>
 
-        <ProductCard />
-      </div>
+            <div
+              style={{ color: "white", fontSize: "16px", marginRight: "130px" }}
+            >
+              Sustainable transport made easy. Your green ride awaits!
+            </div>
+
+            <Link to="/productpage">
+              <button
+                style={{
+                  fontFamily: "Red Hat Display",
+                  fontSize: "14px",
+                  lineHeight: "19px",
+                  textAlign: "left",
+                  verticalAlign: "top",
+                  color: "#faf9f8",
+                  width: "109px",
+                  height: "39px",
+                  borderRadius: "16px",
+                  borderColor: "#9ae5bd",
+                  backgroundColor: "transparent",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: "20px",
+                }}
+              >
+                Explore more
+              </button>
+            </Link>
+          </div>
+
+          <ProductCard />
+        </div>
+      </Main>
     </>
   );
 }
 
 const Main = styled.div`
-  background-color: red;
+  .hero-image {
+    width: 1460px;
+    height: 1044px;
+    z-index: 0;
+  }
+
+  .hero-headline {
+    position: "absolute";
+    top: "352px";
+    left: "301px";
+    width: "873px";
+    height: "306px";
+    background-color: "rgba(0, 0, 0, 0.7)"; // svart färg med 70% opacitet
+    border: "2px solid rgba(0, 0, 0, 0.7)";
+    display: "flex";
+    flex-direction: "column";
+    align-items: "center";
+    justify-content: "center";
+  }
 `;
