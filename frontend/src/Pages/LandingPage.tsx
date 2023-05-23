cba19982974aebaa585a9dfae884d31a222dd6e8
Jovan <jovan.14@hotmail.com>
2023-05-23 13:02:15 +0200

    CSS for landingPage

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -7,49 +7,147 @@ export default function LandingPage() {
   return (
     <>
       <Main>
-        <div style={{ position: "relative", fontFamily: "Red Hat Display" }}>
+        <div className="hero-bg-filler">
           <div className="hero-image">
             <div className="hero-text-banner">
-              <div>Silent. Green. Ecowheelz.</div>
+              <h1 className="hero-headline">Silent. Green. Ecowheelz.</h1>
 
-              <div>
-                Sustainable transport made easy. Your green ride awaits!
+              <div className="slogan-container">
+                <span className="slogan">
+                  Sustainable transport made easy.{" "}
+                  <span className="slogan-br">Your green ride awaits!</span>
+                </span>
+
+                <Link to="/productpage">
+                  <button className="explore-btn">Explore more</button>
+                </Link>
               </div>
-
-              <Link to="/productpage">
-                <button>Explore more</button>
-              </Link>
             </div>
           </div>
-
-          <ProductCard />
         </div>
+        <ProductCard />
       </Main>
     </>
   );
 }
 
 const Main = styled.div`
+  .hero-bg-filler {
+    background-color: #080d11;
+    width: 100%;
+  }
+
   .hero-image {
-    background: url(${HeroImg}), #080d11;
+    background: url(${HeroImg});
     background-repeat: no-repeat;
     background-position: center center;
-    background-size: contain;
+    background-size: cover;
+    max-width: 1630px;
     height: calc(100dvh - 92px);
-    max-height: 1024px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin: auto;
+  }
+
+  .hero-text-banner {
+    background: rgba(0, 0, 0, 0.7);
+    padding: 0 35px;
+    height: 310px;
+    font-family: "Red Hat Display";
+    color: white;
+    font-weight: 700;
+
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
   }
 
   .hero-headline {
-    position: "absolute";
-    top: "352px";
-    left: "301px";
-    width: "873px";
-    height: "306px";
-    background-color: "rgba(0, 0, 0, 0.7)"; // svart färg med 70% opacitet
-    border: "2px solid rgba(0, 0, 0, 0.7)";
-    display: "flex";
-    flex-direction: "column";
-    align-items: "center";
-    justify-content: "center";
+    margin: 0;
+    font-size: 64px;
+    line-height: 75px;
+  }
+
+  .slogan {
+    display: inline-block;
+    margin: 0;
+    font-size: 16px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .explore-btn {
+    background-color: transparent;
+    border: 2px solid #9ae5bd;
+
+    padding: 10px 12px;
+    border-radius: 20px;
+    margin: 0 12px;
+
+    color: white;
+    font-size: 16px;
+    font-weight: 700;
+
+    cursor: pointer;
+  }
+
+  @media (max-width: 815px) {
+    .hero-image {
+      display: block;
+      position: relative;
+    }
+
+    .hero-text-banner {
+      position: absolute;
+      top: 18%;
+      right: 0;
+      left: 0;
+      padding-bottom: 20px;
+
+      height: 260px;
+    }
+
+    .hero-headline {
+      font-size: 48px;
+      line-height: 60px;
+    }
+
+    .slogan {
+      font-size: 14px;
+
+      .slogan-br {
+        display: block;
+      }
+    }
+
+    .explore-btn {
+      display: block;
+      position: absolute;
+      left: 50%;
+      bottom: 12px;
+      transform: translateX(-50%);
+    }
+  }
+
+  @media (max-width: 475px) {
+    .hero-text-banner {
+      display: block;
+      padding-top: 16px;
+      height: 240px;
+    }
+
+    .hero-headline {
+      font-size: 44px;
+      line-height: 55px;
+    }
+
+    .explore-btn {
+      bottom: 35px;
+    }
   }
 `;
