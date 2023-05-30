4e9f641d5cc8dfef9264134703948362ed889546
sannaasp <sanna.asp@hotmail.com>
2023-05-30 09:57:07 +0200

    update

--- frontend/src/components/FooterMobile.tsx
+++ frontend/src/components/FooterMobile.tsx
@@ -21,46 +21,62 @@ function FooterMobile() {
   };
   return (
     <>
+      {/* <div className="footericon-box"> */}
       <div className="footer-box">
-        <div className="footer-dropdownbox">
-          <div className="footer-textheader" onClick={toggleContactDetails}>
-            <p className="footer-text">Contact</p>
-            <div className="footer-iconarrow">
-              <img src="./icons/footer-arrow.png" alt="" />
-            </div>
-          </div>
+        <div className="footericon-box">
+          <div className="footer-dropdownbox">
+            <div className="footer-textheader" onClick={toggleContactDetails}>
+              <p className="footer-text">Contact</p>
 
-          {showContact && (
-            <div className="detail-info">
-              <div className="icon-text">
-                <div className="icon-container">
-                  <img src="./icons/phone.png" alt="" className="footer-icon" />
-                </div>
-                <p className="footer-text">Work with us</p>
-              </div>
-              <div className="icon-text">
-                <div className="icon-container">
-                  <img
-                    src="./icons/person.png"
-                    alt=""
-                    className="footer-icon"
-                  />
-                </div>
-                <p className="footer-text">Privacy policy</p>
-              </div>
-              <div className="icon-text">
-                <div className="icon-container">
-                  <img src="./icons/mail.png" alt="" className="footer-icon" />
-                </div>
-                <p className="footer-text">Terms and conditions</p>
+              <div className="footer-iconarrow">
+                <img src="./icons/footer-arrow.png" alt="" />
               </div>
             </div>
-          )}
+
+            {showContact && (
+              <div className="detail-info">
+                <div className="icon-text">
+                  <div className="icon-container">
+                    <img
+                      src="./icons/phone.png"
+                      alt=""
+                      className="footer-icon"
+                    />
+                  </div>
+                  <p className="footer-text">Work with us</p>
+                </div>
+                <div className="icon-text">
+                  <div className="icon-container">
+                    <img
+                      src="./icons/person.png"
+                      alt=""
+                      className="footer-icon"
+                    />
+                  </div>
+                  <p className="footer-text">Privacy policy</p>
+                </div>
+                <div className="icon-text">
+                  <div className="icon-container">
+                    <img
+                      src="./icons/mail.png"
+                      alt=""
+                      className="footer-icon"
+                    />
+                  </div>
+                  <p className="footer-text">Terms and conditions</p>
+                </div>
+              </div>
+            )}
+          </div>
+          {/* <div className="footer-logo">
+            <img src="./logo/Logo-desktop.png" alt="icon" />
+          </div> */}
         </div>
 
         <div className="footer-dropdownbox">
           <div className="footer-textheader" onClick={toggleAboutusDetails}>
             <p className="footer-text">About us</p>
+            {/* <img src="" alt="" /> */}
             <div className="footer-iconarrow">
               <img src="./icons/footer-arrow.png" alt="" />
             </div>
