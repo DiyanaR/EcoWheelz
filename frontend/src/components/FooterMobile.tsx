7f687f6998dc0c8bbb3ec2d085fa7cb0b8b636bd
sannaasp <sanna.asp@hotmail.com>
2023-05-30 14:43:13 +0200

    footer

--- frontend/src/components/FooterMobile.tsx
+++ frontend/src/components/FooterMobile.tsx
@@ -24,95 +24,111 @@ function FooterMobile() {
     <>
       {/* <div className="footericon-box"> */}
       <div className="footer-box">
-        <div className="footericon-box">
-          <div className="footer-dropdownbox">
-            <div className="footer-textheader" onClick={toggleContactDetails}>
-              <p className="footer-text">Contact</p>
+        <div className="footer-secondbox">
+          <div className="footericon-box">
+            <div className="footer-dropdownbox">
+              <div className="footer-textheader" onClick={toggleContactDetails}>
+                <p className="footer-text">Contact</p>
 
-              <div className="footer-iconarrow">
-                <img src="./icons/footer-arrow.png" alt="" />
-              </div>
-            </div>
-
-            {showContact && (
-              <div className="detail-info">
-                <div className="icon-text">
-                  <div className="icon-container">
-                    <img
-                      src="./icons/phone.png"
-                      alt=""
-                      className="footer-icon"
-                    />
-                  </div>
-                  <p className="footer-text">Work with us</p>
-                </div>
-                <div className="icon-text">
-                  <div className="icon-container">
-                    <img
-                      src="./icons/person.png"
-                      alt=""
-                      className="footer-icon"
-                    />
-                  </div>
-                  <p className="footer-text">Privacy policy</p>
-                </div>
-                <div className="icon-text">
-                  <div className="icon-container">
-                    <img
-                      src="./icons/mail.png"
-                      alt=""
-                      className="footer-icon"
-                    />
-                  </div>
-                  <p className="footer-text">Terms and conditions</p>
+                <div className="footer-iconarrow">
+                  <img src="./icons/footer-arrow.png" alt="" />
                 </div>
               </div>
-            )}
-          </div>
-          <div className="logo-mobile">
-            <img src="./logo/Logo-desktop.png" alt="logo" />
-          </div>
-        </div>
 
-        <div className="footer-dropdownbox">
-          <div className="footer-textheader" onClick={toggleAboutusDetails}>
-            <p className="footer-text">About us</p>
-            {/* <img src="" alt="" /> */}
-            <div className="footer-iconarrow">
-              <img src="./icons/footer-arrow.png" alt="" />
+              {showContact && (
+                <div className="detail-info">
+                  <div className="icon-text">
+                    <div className="icon-container">
+                      <img
+                        src="./icons/phone.png"
+                        alt=""
+                        className="footer-icon"
+                      />
+                    </div>
+                    <p className="footer-text">Work with us</p>
+                  </div>
+                  <div className="icon-text">
+                    <div className="icon-container">
+                      <img
+                        src="./icons/person.png"
+                        alt=""
+                        className="footer-icon"
+                      />
+                    </div>
+                    <p className="footer-text">Privacy policy</p>
+                  </div>
+                  <div className="icon-text">
+                    <div className="icon-container">
+                      <img
+                        src="./icons/mail.png"
+                        alt=""
+                        className="footer-icon"
+                      />
+                    </div>
+                    <p className="footer-text">Terms and conditions</p>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="logo-mobile">
+              <img
+                className="footer-icons"
+                src="./logo/Logo-desktop.png"
+                alt="logo"
+              />
             </div>
           </div>
-          {showAbout && (
-            <div className="detail-info">
-              <p className="footer-text"> + 46 745 343 21</p>
-              <p className="footer-text">Gloryroad1</p>
-              <p className="footer-text">service@eccowheelz.com</p>
-            </div>
-          )}
-        </div>
-        <div className="logo-mobile">
-          <img src="./icons/fb.png" alt="logo" />
-        </div>
 
-        <div className="footer-dropdownbox">
-          <div className="footer-textheader" onClick={toggleFaqDetails}>
-            <p className="footer-text">FAQ</p>
-            <div className="footer-iconarrow">
-              <img src="./icons/footer-arrow.png" alt="" />
+          <div className="footericon-box">
+            <div className="footer-dropdownbox">
+              <div className="footer-textheader" onClick={toggleAboutusDetails}>
+                <p className="footer-text">About us</p>
+                {/* <img src="" alt="" /> */}
+                <div className="footer-iconarrow">
+                  <img src="./icons/footer-arrow.png" alt="" />
+                </div>
+              </div>
+              {showAbout && (
+                <div className="detail-info">
+                  <p className="footer-text"> + 46 745 343 21</p>
+                  <p className="footer-text">Gloryroad1</p>
+                  <p className="footer-text">service@eccowheelz.com</p>
+                </div>
+              )}
+            </div>
+            <div className="logo-mobile">
+              <img className="footer-icons" src="./icons/fb.png" alt="logo" />
             </div>
           </div>
-          {showFAQ && (
-            <div className="detail-info">
-              <p className="footer-text">Shipping and returns</p>
-              <p className="footer-text">Store policy</p>
-              <p className="footer-text">Payments</p>
+
+          <div className="footericon-box">
+            <div className="footer-dropdownbox">
+              <div className="footer-textheader" onClick={toggleFaqDetails}>
+                <p className="footer-text">FAQ</p>
+                <div className="footer-iconarrow">
+                  <img src="./icons/footer-arrow.png" alt="" />
+                </div>
+              </div>
+              {showFAQ && (
+                <div className="detail-info">
+                  <p className="footer-text">Shipping and returns</p>
+                  <p className="footer-text">Store policy</p>
+                  <p className="footer-text">Payments</p>
+                </div>
+              )}
             </div>
-          )}
-          <div className="logo-mobile">
-            <img src="./icons/instagram.png" alt="logo" />
+            <div className="logo-mobile">
+              <img
+                className="footer-icons"
+                src="./icons/instagram.png"
+                alt="logo"
+              />
+            </div>
           </div>
+          <p className="copyright-text">
+            2023 Ecowheelz AB. All rights reserved
+          </p>
         </div>
-        <p className="copyright-text">2023 Ecowheelz AB. All rights reserved</p>
       </div>
     </>
   );
